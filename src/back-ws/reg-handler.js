b17e3e3492bb3ac3159d7b72a88d77a571851aa3
NickUA <246246s@gmail.com>
2023-07-07 22:43:30 +0300

    feat: add ws.id to user entity binded with connection

--- src/back-ws/reg-handler.js
+++ src/back-ws/reg-handler.js
@@ -1,18 +1,29 @@
-// type "reg" handler  
+import { userRepository } from './db/db.js';
+
+// type "reg" handler:
 export function handleRegistration(ws, data, id) {
-  // registration logic ???
-  console.log('Received reg message:', data);
+  console.log('Received reg message:', data, ws.id);
+  let connectedUser;
+  connectedUser = userRepository.getByName(data.name);
+  if (connectedUser) {
+    // check password
+    connectedUser = userRepository.update(connectedUser.id, { ...connectedUser, wsId: ws.id });
+  }
+  else connectedUser = userRepository.create({ ...data, wsId: ws.id });
+  console.log(connectedUser);
 
   const response = {
     type: 'reg',
     data: JSON.stringify({
-      name: data.name,
-      index: 1,
+      name: connectedUser.name,
+      index: connectedUser.id,
       error: false,
       errorText: '',
     }),
     id,
   };
+
+  console.log('total users: ' + userRepository.getAll().length);
 
   ws.send(JSON.stringify(response));
 }
--- src/back-ws/websocket-server.js
+++ src/back-ws/websocket-server.js
@@ -1,9 +1,11 @@
+import { randomUUID } from 'node:crypto';
 import { WebSocketServer } from 'ws';
 import { handleRegistration } from './reg-handler.js';
+import { createRoom } from './room-handler.js';
 
 const messageHandlers = {
   reg: handleRegistration,
-  create_room: () => { },
+  create_room: createRoom,
   add_user_to_room: () => { },
   add_ships: () => { },
   attack: () => { },
@@ -14,17 +16,21 @@ export const startWebSocketServer = (port) => {
   const wsServer = new WebSocketServer({ port });
   console.log(`WebSocket server started on the ${port}`);
 
-  wsServer.on('connection', function connection(ws) {
+  wsServer.on('connection', ws => {
+    // console.log('Clients: ', wsServer.clients.size);
+    ws.id = randomUUID();
     ws.on('error', console.error);
 
     ws.on('message', function message(msg) {
       try {
         const parsedMessage = JSON.parse(msg);
         console.log(parsedMessage);
-        const { type, data, id } = parsedMessage;
+        let { type, data, id } = parsedMessage;
 
-        if (messageHandlers[type]) {
-          messageHandlers[type](ws, data, id);
+        if (data.length > 0) data = JSON.parse(data); // feature on the frontend side
+
+        if (messageHandlers[type] && id === 0) {
+          messageHandlers[type](ws, data);
         } else {
           console.log(`No handler found for message type: ${type}`);
         }
