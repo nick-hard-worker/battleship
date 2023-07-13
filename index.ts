2a26334e35576a1821d65497d5097fca02e1a5ee
NickUA <246246s@gmail.com>
2023-07-13 21:18:49 +0300

    feat: add disconected websocket client handler

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { httpServer } from "./src/http_server/index.js";
-import { startWebSocketServer } from './src/back-ws/websocket-server.js';
+import { ExtendedWebSocket, startWebSocketServer } from './src/back-ws/websocket-server.js';
 import 'dotenv/config';
 
 const HTTP_PORT = process.env.HTTP_PORT || 8181;
@@ -8,3 +8,17 @@ const PORT_WS = process.env.PORT_WS || 3000;
 export const wsServer = startWebSocketServer(+PORT_WS);
 httpServer.listen(+HTTP_PORT);
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
+
+const interval = setInterval(function ping() {
+  wsServer.clients.forEach(function each(ws) {
+    const wsExt = ws as ExtendedWebSocket;
+    if (wsExt.isAlive === false) return wsExt.terminate();
+
+    wsExt.isAlive = false;
+    wsExt.ping();
+  });
+}, 10000);
+
+wsServer.on('close', function close() {
+  clearInterval(interval);
+});
--- src/back-ws/websocket-server.ts
+++ src/back-ws/websocket-server.ts
@@ -4,9 +4,13 @@ import { handleRegistration } from './request-handlers/reg-handler.js';
 import { createRoom, addUserToRoom } from './request-handlers/room-handler.js';
 import { addShips } from './request-handlers/ships-handler.js';
 import { attack } from './request-handlers/attack-handler.js';
+import { wsServer } from '../../index.js';
+import { gameRepository, roomRepository, userRepository } from './db/db.js';
 
 export interface ExtendedWebSocket extends WebSocket {
   id: string;
+  isAlive: boolean;
+  heartbeat(ws: WebSocket): void;
 }
 
 type IInputTypeMsg = 'reg' | 'create_room' | 'add_user_to_room' | 'add_ships' | 'attack' | 'randomAttack';
@@ -26,7 +30,12 @@ export const startWebSocketServer = (port: number) => {
   console.log(`WebSocket server started on the ${port}`);
 
   wsServer.on('connection', (ws: ExtendedWebSocket) => {
+    ws.heartbeat = function (this: ExtendedWebSocket) {
+      this.isAlive = true;
+    }
     ws.id = randomUUID();
+    ws.isAlive = true;
+    ws.on('pong', ws.heartbeat);
     ws.on('error', console.error);
 
     ws.on('message', function message(msg) {
@@ -45,8 +54,31 @@ export const startWebSocketServer = (port: number) => {
         console.error('Failed to parse incoming message:', error);
       }
     });
+
+    ws.on('close', () => {
+      console.log(`client terminated ${ws.id}`)
+      const disconnectedUser = userRepository.getByWsId(ws.id);
+
+      // delete existing rooms with user
+      const rooms = roomRepository
+        .getAll()
+        .filter(room => room.roomUsers.some(user => user.index === disconnectedUser?.id));
+      console.log(rooms);
+      rooms.forEach(room => roomRepository.delete(room.id as number));
+      console.log(roomRepository.getAll());
+
+      // delete existing games with user
+      const games = gameRepository
+        .getAll()
+        .filter(game => game.players.some(user => user.userId === disconnectedUser?.id));
+      console.log(games);
+      games.forEach(game => gameRepository.delete(game.id as number));
+      console.log(gameRepository.getAll());
+    });
   });
   return wsServer;
+
+
 };
 
 function isMessageHandler(type: any): type is IInputTypeMsg {
